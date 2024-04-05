35d45926ca52b7c7f33706f70ea20d2d5d9e6974
tmanso9 <tmanso92@gmail.com>
2024-04-05 20:32:26 +0100

    navlink as separate component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link'
 
 export function Header() {
 	return (
 		<div className="flex items-center gap-5 py-2">
 			<img src={nlwUniteIcon} />
-			<nav className='flex gap-5'>
-				<a className='font-medium text-sm text-zinc-300' href="">Events</a>
-				<a className='font-medium text-sm ' href="">Attendees</a>
+			<nav className="flex gap-5">
+				<NavLink href="/events">Events</NavLink>
+				<NavLink href="/attendees">Attendees</NavLink>
 			</nav>
 		</div>
 	)
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,9 @@
+import { ComponentProps } from 'react'
+
+interface NavLinkProps extends ComponentProps<'a'> {}
+
+export function NavLink(props: NavLinkProps) {
+	return (
+		<a {...props} className="font-medium text-sm " />
+	)
+}
